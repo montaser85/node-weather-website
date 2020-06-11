918016237d96c6084d832d5c4242e39f6ca655f3
montaser85 <montamcc@gmail.com>
2020-06-12 01:06:16 +0600

    Seeting up for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const forecast=require('./utils/forecast')
 
 const app= express()
 
+const port=process.env.PORT || 3000
+
 //Define paths for express config
 const publicDirectoryPath=path.join(__dirname, '../public')
 const viewsDirectory =path.join(__dirname, '../templates/views')
@@ -100,6 +102,6 @@ app.get(
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port '+port)
 })
